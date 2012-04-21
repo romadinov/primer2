8072c2778989c5585a0de2fe16106f2fc8a368cf
Alexey Romadinov <Alexey.Romadinov@gmail.com>
2012-04-21 23:49:37 +1000

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Primer2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Primer2::Application.config.secret_token = '637cc0bab8e8e342bbb06ae95f65e9973e576ffbcdeab207caa1d1b64c0ad0a3d1c7af8c43df711b4a0770b0e3431d1d0914d2da24e1b948ae3b66ea6604cb66'
